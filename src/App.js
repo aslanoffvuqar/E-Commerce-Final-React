017662f3e124d0d9b1e46cc61ccd946dc5fd51a2
aliyev-orxan301992 <orxan301992@gmail.com>
2024-05-18 22:37:40 +0400

    login dizayn

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Nav from "./Components/Foldder-Nav/Nav";
 import Footer from "./Components/Foldder-Footer/Footer";
 import Home from "./Components/Home";
 import Aside from "./Components/Foldder-Aside/Aside";
+import Login from "./Components/Login/Login";
+
+
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +17,7 @@ function App() {
         <Route path="/Home" element={<Home />} />
       </Routes>
       <Aside/>
+      <Login/>
       <Footer />
     </BrowserRouter>
   );
