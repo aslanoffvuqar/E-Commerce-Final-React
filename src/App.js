31472d24a8904d0f4f6c2864af45905d3c4cf918
feridkazimov <feridkazimov0021@gmail.com>
2024-07-01 14:32:40 +0400

    UserHome

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import Nav from "./Components/NavBar/Nav";
 import Footer from "./Components/FooterBar/Footer";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
-import SignUp from "./Components/Sign-up/Sign_up"; // Обновленный путь
-// import Aside from "./Components/AsideBar/Aside";
+import SignUp from "./Components/Sign-up/Sign_up";
 import Aside from "./Components/AsideBar/Aside";
-// import Login from "./Components//Login/Login";
 import About from "./Components/AboutBar/About";
 import Section_one from "./Components/Section_one_main/Section_one";
 import Categories from "./Components/Categories/Categories";
@@ -18,29 +16,33 @@ import BestProducts from "./Components/Best_product/BestProduct";
 import ExploreProduct from "./Components/Explore_product/ExploreProduct";
 import Contact from "./Components/Contact/Contact";
 import Product_page from "./Components/Product_page/Product_page";
+import UserHome from "./Components/UserHome/UserHome";                                                                
 import "./i18n";
 
 function AppContent() {
   const location = useLocation();
+  const isUserHome = location.pathname === "/UserHome";
+  
   return (
     <>
-      <Header />
-      <Nav />
+    
+      {!isUserHome && <Header />}
+      {!isUserHome && <Nav />}
       <Routes>
         <Route path="/Home" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Login" element={<Login />} />
-        <Route path="/SignUp" element={<SignUp />} /> 
+        <Route path="/SignUp" element={<SignUp />} />
         <Route path="/ViewAllProduct" element={<Product_page />} />
+        <Route path="/UserHome" element={<UserHome />} /> 
       </Routes>
 
-      {location.pathname !== "/About" &&
+      {!isUserHome && location.pathname !== "/About" &&
         location.pathname !== "/Contact" &&
         location.pathname !== "/Login" &&
-        location.pathname !== "/SignUp" && 
+        location.pathname !== "/SignUp" &&
         location.pathname !== "/ViewAllProduct" && (
-
           <>
             <Aside />
             <Section_one />
@@ -51,13 +53,13 @@ function AppContent() {
             <NewArrival />
           </>
         )}
-      <Footer />
+        
+      {!isUserHome && <Footer />}
     </>
   );
 }
 
 function App() {
-
   return (
     <BrowserRouter>
       <AppContent />
